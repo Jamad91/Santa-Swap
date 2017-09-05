be4cadf876c3c973dbdba1b8a0569ad31985c470
Jimmy <jdicolandrea@gmail.com>
2017-09-05 00:06:50 -0400

    create list on click

--- app/components/Exchange/ExchangeList.jsx
+++ app/components/Exchange/ExchangeList.jsx
@@ -17,39 +17,35 @@ class ExchangeList extends Component {
   }
 
   matchMaker(arr) {
-    let newArr
-    arr
-      ? newArr = this.shuffle(arr.slice())
-      : newArr = []
-    let exchange = [];
-    for (var i = 0; i < newArr.length; i++) {
-      if (arr[i] === newArr[i]) {
-        newArr = this.shuffle(newArr);
-        i = -1;
-        exchange = [];
-      } else {
-        exchange.push({ giver: arr[i], receiver: newArr[i] });
-      }
+  let newArr = this.shuffle(arr.slice());
+  let exchange = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].id === newArr[i].id) {
+      newArr = this.shuffle(newArr);
+      i = -1;
+      exchange = [];
+    } else {
+      exchange.push({ giver: arr[i], receiver: newArr[i] });
     }
-    this.setState({
-      list: exchange
-    })
-    return exchange;
   }
+  console.log(exchange);
+  return exchange;
+}
 
-  goodMatch(el1, el2, arr) {
-    for (var i = 0; i < arr.length; i++) {
-      if(arr[i].includes(el1) && arr[i].includes(el2)) {return false}
-    }
-    return true;
-  }
+  // goodMatch(el1, el2, arr) {
+  //   for (var i = 0; i < arr.length; i++) {
+  //
+  //     if(arr[i].includes(el1) && arr[i].includes(el2)) {return false}
+  //   }
+  //   return true;
+  // }
 
-  shuffle(arr) {
-    for (let i = arr.length; i; i--) {
+  shuffle(a) {
+    for (let i = a.length; i; i--) {
       let j = Math.floor(Math.random() * i);
-      [arr[i - 1], arr[j]] = [arr[j], arr[i - 1]];
+      [a[i - 1], a[j]] = [a[j], a[i - 1]];
     }
-    return arr;
+    return a;
   }
 
   displayPeople(idArr, memberArr) {
@@ -72,15 +68,18 @@ class ExchangeList extends Component {
   }
 
   render() {
-    let matchUps = this.displayPeople(this.state.list, this.props.users);
+    console.log('this', this.props);
+    console.log('check', this.state.list, this.props.users);
+    let matchUps = this.matchMaker(this.props.members);
+    console.log('matchUps',matchUps);
     return (
       <div>
         <button>Generate List</button><br />
         {
           matchUps.map(match =>
-            <div key={match.giver.name}>
-              <span>Giver: {match.giver.name}</span><br />
-              <span>Receiver: {match.receiver.name}</span>
+            <div key={match.giver.id}>
+              <span>Giver: {match.giver.firstName}</span><br />
+              <span>Receiver: {match.receiver.firstName}</span>
               <hr />
             </div>
           )
@@ -92,73 +91,8 @@ class ExchangeList extends Component {
 
 function mapStateToProps(state) {
   return {
-    // auth: state.auth,
-    // exchange: state.exchangeReducer.selectedExchange,
-    users: state.userReducer.users
+
   }
 }
 
 export default connect(mapStateToProps)(ExchangeList)
-
-// let members = [
-//   { id: 1, name: 'Jimmy' },
-//   { id: 2, name: 'Joey' },
-//   { id: 3, name: 'tiara' },
-//   { id: 4, name: 'Katie' },
-//   { id: 5, name: 'DiCo' },
-//   { id: 6, name: 'Devon' },
-//   { id: 7, name: 'Gary' },
-//   { id: 8, name: 'Andrew' },
-//   { id: 9, name: 'Matt' },
-//   { id: 10, name: 'Mitalee' },
-//   { id: 11, name: 'Hayley' },
-// ];
-
-// badMatches = [
-//   [{ id: 9, name: 'Matt' }, { id: 10, name: 'Mitalee' }],
-//   [
-//     { id: 1, name: 'Jimmy' },
-//     { id: 2, name: 'Joey' },
-//     { id: 3, name: 'tiara' },
-//     { id: 4, name: 'Katie' },
-//     { id: 5, name: 'DiCo' },
-//   ],
-//   [{ id: 6, name: 'Devon' }, { id: 8, name: 'Andrew' }],
-// ];
-
-// let memberIds = [1,2,3,4,5,6,7,8,9,10,11]
-
-// let badMatches = [[9,10],[1,2,3,4,5],[6,8]]
-/*
-function matchMaker(arr) {
-  let newArr = shuffle(arr.slice());
-  let exchange = [];
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === newArr[i] && goodMatch(arr[i], newArr[i], badMatches)) {
-      newArr = shuffle(newArr);
-      i = -1;
-      exchange = [];
-    } else {
-      exchange.push({ giver: arr[i], receiver: newArr[i] });
-    }
-  }
-  return exchange;
-}
-
-function goodMatch(el1, el2, arr) {
-  for (var i = 0; i < arr.length; i++) {
-
-    if(arr[i].includes(el1) && arr[i].includes(el2)) {return false}
-  }
-  return true;
-}
-
-function shuffle(a) {
-  for (let i = a.length; i; i--) {
-    let j = Math.floor(Math.random() * i);
-    [a[i - 1], a[j]] = [a[j], a[i - 1]];
-  }
-  return a;
-}
-console.log(matchMaker(memberIds));
-*/
--- app/components/Exchange/SingleExchange.jsx
+++ app/components/Exchange/SingleExchange.jsx
@@ -2,10 +2,32 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import store from 'APP/app/store'
 import { Link } from 'react-router'
-import {removeMember} from 'APP/app/reducers/exchanges'
+import {removeMember, makeList} from 'APP/app/reducers/exchanges'
 import ExchangeList from './ExchangeList'
 
 class SingleExchange extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      userIncluded: false,
+      madeList: false
+    }
+
+    if (this.props.list) {
+      this.setState({madeList: true})
+    }
+
+    this.makeList = this.makeList.bind(this)
+  }
+
+  makeList() {
+    // if (this.props.user)
+
+    console.log('making list');
+    this.setState({madeList: true})
+  }
 
   render() {
     let exchange = this.props.exchange
@@ -18,17 +40,27 @@ class SingleExchange extends Component {
             ? exchange.members.map(member =>
               <div key={member.id}>
                 {member.firstName} {member.lastName}
-                <span onClick={() => {
-                    window.location.reload()
-                    this.props.removeMember(exchange.id, member.id)
-                  }
-                }>X</span>
+                {
+                  this.props.auth
+                    ? <span onClick={() => {
+                        window.location.reload()
+                        this.props.removeMember(exchange.id, member.id)
+                      }
+                    }>X</span>
+                  : null
+                }
 
               </div>
             )
             : null
         }
-        <Link href={`/exchanges/${exchange.id}/join`}>Join</Link><br />
+        {
+          this.state.madeList
+          ? <ExchangeList members={exchange.members}/>
+          : <div onClick={this.makeList}>Make list</div>
+        }
+
+        <Link href={`/exchanges/${exchange.id}/join`}>Join Link</Link><br />
         <Link href="/home">Home</Link>
       </div>
     )
@@ -43,4 +75,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, {removeMember})(SingleExchange)
+export default connect(mapStateToProps, {removeMember, makeList})(SingleExchange)
