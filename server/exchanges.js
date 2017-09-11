a06df3a298900893697d439507b1b62b89b07a50
Jimmy <jdicolandrea@gmail.com>
2017-09-11 19:42:43 -0400

    set up multiple emails

--- server/exchanges.js
+++ server/exchanges.js
@@ -25,6 +25,18 @@ router.put('/:id', (req, res, next) => {
   Exchange.findById(req.params.id)
     .then(exchange => {
       console.log('REQ.BODY', Object.keys(req.body[0])[0]);
+      console.log('REQ.BODY', req.body);
+      let mailList = [];
+      let names = []
+
+      for (var i = 0; i < req.body.length; i++) {
+        mailList.push(req.body[i].giver.email)
+        names.push(req.body[i].giver.firstName)
+      }
+
+      console.log('MAIL LIST', mailList);
+      console.log('NAMES', names);
+
       let newMembers = exchange.members
       let newList = exchange.list
       if (Object.keys(req.body[0])[0] === 'giver') {
@@ -36,24 +48,23 @@ router.put('/:id', (req, res, next) => {
           //login_info is an exported object with a user and a password key
           // info corresponds to a google account, in this case santaswap25
         });
-        var text = 'Hello world from Jimmy!'
-
-        var mailOptions = {
-          from: 'santaswap25@gmail.com', // sender address
-          to: 'jdicolandrea@gmail.com', // list of receivers
-          subject: 'Email Example', // Subject line
-          text: text //, // plaintext body
-          // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
-        };
-
-        transporter.sendMail(mailOptions, function(error, info){
-          if(error){
+        for (var i = 0; i < req.body.length; i++) {
+          let mailOptions = {
+            from: 'santaswap25@gmail.com',
+            to: mailList[i],
+            subject: 'Email Example',
+            text: `Hello ${names[i]}`
+          }
+          transporter.sendMail(mailOptions, function(error, info){
+            if(error){
               console.log('ERROR:', error);
-          }else{
+            }else{
               console.log('Message sent: ' + info.response);
               res.json({yo: info.response});
-          };
-        });
+            };
+          });
+        }
+
 
       }
       else if (Object.keys(req.body).length > 1) {
