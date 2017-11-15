dd948c2794a0b00f86d9ef63eb1e754bbd2d2537
Jimmy <jdicolandrea@gmail.com>
2017-11-14 21:37:50 -0500

    add santa face features

--- app/components/SantaBox.jsx
+++ app/components/SantaBox.jsx
@@ -10,7 +10,16 @@ export default class SantaBox extends Component {
           <div id="hat-body"></div>
           <div id="hat-brim"></div>
         </div>
-        <div id="santa-head"></div>
+        <div id="santa-head">
+          <div id="left-eye">
+            <div className="eye-glimmer"></div>
+          </div>
+          <div id="right-eye">
+            <div className="eye-glimmer"></div>
+          </div>
+          <div id="nose"></div>
+          <div id="beard"></div>
+        </div>
       </div>
     )
   }
