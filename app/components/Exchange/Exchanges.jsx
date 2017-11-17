efb4d40ca23f9296ddbca0482b745f734f69d3e2
Jimmy <jdicolandrea@gmail.com>
2017-11-17 18:54:04 -0500

    users can only see their own exchanges

--- app/components/Exchange/Exchanges.jsx
+++ app/components/Exchange/Exchanges.jsx
@@ -9,17 +9,21 @@ class Exchanges extends Component {
     return (
       <div className="content-box" id="managing">
         <h1 className="header-font">Exchanges I'm managing</h1>
-        {exchanges.map(exchange =>
-          <div key={`${exchange.title}`} className="listing">
-            <Link href={`/exchanges/${exchange.id}`} style={{ textDecoration: 'none' }}>
-              <div>
-                <h2 className="header-font">{exchange.title}</h2>
-                <p className="exchange-info">Members: {exchange.members.length}</p>
-                <p className="exchange-info">List Made: {exchange.list.length > 0 ? <span>Yes</span> : <span>No</span>}
-                </p>
+        {exchanges.map(exchange => {
+          if (this.props.user.id === exchange.owner_id) {
+            return (
+              <div key={`${exchange.title}`} className="listing">
+                <Link href={`/exchanges/${exchange.id}`} style={{ textDecoration: 'none' }}>
+                  <div>
+                    <h2 className="header-font">{exchange.title}</h2>
+                    <p className="exchange-info">Members: {exchange.members.length}</p>
+                    <p className="exchange-info">List Made: {exchange.list.length > 0 ? <span>Yes</span> : <span>No</span>}
+                    </p>
+                  </div>
+                </Link>
               </div>
-            </Link>
-          </div>
+            )
+          }}
         )}
       </div>
     )
