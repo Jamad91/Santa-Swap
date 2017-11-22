bf9d180b5f1e65d1d23aa6561b742b7d681cf009
Jimmy <jdicolandrea@gmail.com>
2017-11-22 16:54:11 -0500

    styleize submission page

--- app/components/Exchange/Submitted.jsx
+++ app/components/Exchange/Submitted.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import SantaBox from '../SantaBox'
 
 export default class Submitted extends Component {
   render() {
     return(
-      <div>
-        <div>Submitted form! You'll hear back soon!</div>
-        <Link href={'/'}>Home</Link>
+      <div className="page-content" id="submitted">
+        <div className="signedin-body">
+          <h1 className="header-font">Submitted form! Thank you!</h1><br />
+          <h1 className="header-font">Once everybody else has signed up, you'll be getting notified of who you're getting a present for.</h1><br />
+          <h1 className="header-font">So, stay tuned and in the meantime, merry holidays!</h1><br /><br />
+          <div id="submitted-home-link">
+            <Link href={'/'}>Home</Link>
+          </div>
+        </div>
       </div>
     )
   }
