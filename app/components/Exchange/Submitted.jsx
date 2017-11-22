0bc8a8eb9de4b04c940330d49b7a64c07e8cc4e5
Jimmy <jdicolandrea@gmail.com>
2017-11-22 17:25:06 -0500

    add more style to submission page

--- app/components/Exchange/Submitted.jsx
+++ app/components/Exchange/Submitted.jsx
@@ -6,14 +6,12 @@ export default class Submitted extends Component {
   render() {
     return(
       <div className="page-content" id="submitted">
-        <div className="signedin-body">
+        <div id="submitted-body" className="signedin-body">
           <h1 className="header-font">Submitted form! Thank you!</h1><br />
           <h1 className="header-font">Once everybody else has signed up, you'll be getting notified of who you're getting a present for.</h1><br />
-          <h1 className="header-font">So, stay tuned and in the meantime, merry holidays!</h1><br /><br />
-          <div id="submitted-home-link">
-            <Link href={'/'}>Home</Link>
-          </div>
+          <h1 className="header-font">So, stay tuned and in the meantime, <br />merry holidays!</h1><br /><br />
         </div>
+        <SantaBox />
       </div>
     )
   }
